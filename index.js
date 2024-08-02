ad666d1b1baac455a8b5bcb39f7de013442651b8
nasultana89 <nahidgnzts@yahoo.com>
2024-08-01 23:58:41 -0400

    comment

--- index.js
+++ index.js
@@ -59,6 +59,87 @@ app.delete('/users/:id', (req, res) => {
     }
 });
 
+
+app.get('/posts', (req, res) => {
+  const { userId } = req.query;
+  if (userId) {
+      const filteredPosts = posts.filter(p => p.userId === parseInt(userId));
+      res.json(filteredPosts);
+  } else {
+      res.json(posts);
+  }
+});
+
+app.post('/posts', (req, res) => {
+  const newPost = { id: posts.length + 1, ...req.body };
+  posts.push(newPost);
+  res.status(201).json(newPost);
+});
+
+app.patch('/posts/:id', (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+  const post = posts.find(p => p.id === parseInt(id));
+  if (post) {
+      Object.assign(post, updates);
+      res.json(post);
+  } else {
+      res.status(404).send('Post not found');
+  }
+});
+
+app.delete('/posts/:id', (req, res) => {
+  const { id } = req.params;
+  const index = posts.findIndex(p => p.id === parseInt(id));
+  if (index !== -1) {
+      const deletedPost = posts.splice(index, 1);
+      res.json(deletedPost);
+  } else {
+      res.status(404).send('Post not found');
+  }
+});
+
+app.get('/comments', (req, res) => {
+  const { postId } = req.query;
+  if (postId) {
+      const filteredComments = comments.filter(c => c.postId === parseInt(postId));
+      res.json(filteredComments);
+  } else {
+      res.json(comments);
+  }
+});
+
+app.post('/comments', (req, res) => {
+  const newComment = { id: comments.length + 1, ...req.body };
+  comments.push(newComment);
+  res.status(201).json(newComment);
+});
+
+app.patch('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+  const comment = comments.find(c => c.id === parseInt(id));
+  if (comment) {
+      Object.assign(comment, updates);
+      res.json(comment);
+  } else {
+      res.status(404).send('Comment not found');
+  }
+});
+
+app.delete('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  const index = comments.findIndex(c => c.id === parseInt(id));
+  if (index !== -1) {
+      const deletedComment = comments.splice(index, 1);
+      res.json(deletedComment);
+  } else {
+      res.status(404).send('Comment not found');
+  }
+});
+
+
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
